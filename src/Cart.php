8a5dca0cc16f20b6b67786f6f0078f759285f16b
abdel <abdel@mail.com>
2020-11-27 10:48:51 +0100

    add count method

--- src/Cart.php
+++ src/Cart.php
@@ -2,7 +2,22 @@
 
 namespace SimpleCart;
 
-class Cart
+class Cart implements \Countable
 {
+    private $products;
 
+    /**
+     * Cart constructor.
+     * @param $products
+     */
+    public function __construct(array $products = [])
+    {
+        $this->products = $products;
+    }
+
+
+    public function count()
+    {
+        return count($this->products);
+    }
 }
