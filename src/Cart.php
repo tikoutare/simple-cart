9631b97e2589ebc667ec30a3f1b1b4fd50e1f66e
abdel <abdel@mail.com>
2020-11-27 11:08:50 +0100

    add some cart methods

--- src/Cart.php
+++ src/Cart.php
@@ -17,10 +17,44 @@ class Cart implements \Countable
 
     /**
      * return the amount of items in it
+     *
      * @return int
      */
-    public function count()
+    public function count(): int
     {
         return count($this->items);
+    }
+
+    /**
+     * add a new item to the cart
+     *
+     * @param object $item
+     * @return $this
+     */
+    public function addItem(object $item): self
+    {
+        $id = spl_object_hash($item);
+        $this->items[$id] = $item;
+
+        return $this;
+    }
+
+    /**
+     * Remove an existing item from the cart
+     *
+     * @param object $item
+     * @return $this
+     */
+    public function removeItem(object $item): self
+    {
+        $id = spl_object_hash($item);
+        unset($this->items[$id]);
+
+        return $this;
+    }
+
+    private function getItemId(object $item)
+    {
+        return spl_object_hash($item);
     }
 }
